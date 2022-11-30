34e05093c7361849ba597d9292fb60502f60af9b
S4ika <s4ikait@gmail.com>
2022-11-30 16:23:05 +0300

    Добавлен вывод времени при правильном вводе всего предложения и мелкие багофиксы =)

--- src/KeyCatcher.java
+++ src/KeyCatcher.java
@@ -9,9 +9,10 @@ public class KeyCatcher extends JFrame implements KeyListener {
     private JTextField txtf = new JTextField();
     private JLabel[] labels = new JLabel[0];
     private int catcher = 0;
-
+    private long startTime;
     private boolean flag = false;
 
+    private boolean wrongLit = false;
     private int labelsSize;
     public KeyCatcher(JTextField smfng, JLabel[] lbls) {
         this.txtf = smfng;
@@ -20,23 +21,34 @@ public class KeyCatcher extends JFrame implements KeyListener {
         this.labelsSize = lbls.length;
     }
 
-//    void KeyCather(JTextField smfng, JLabel [] lbls){
-//            this.txtf = smfng;
-//            this.labels = lbls;
-//    }
-//    public void cleanArea(){
-//        txtf.setText("");
-//    }
-
     public void setCatcher(int catcher1){
         this.catcher = catcher1;
     }
-
+    //Возвращаем количество введенных правильно букв
     public int getCatcher(){
         return this.catcher;
     }
+    //Обнуляем таймер
+    public void killTimer(){this.flag = false;}
+    //Получает на вход время и возвращает строку в "красивом виде"
+    private String timeMillstoSecAndMin(int ourTime){
+        String time = "";
+        ourTime = (int) ourTime / 1000;
+        if (ourTime/60 > 0) {
+            time = Integer.toString(ourTime / 60) + " минут(ы) " + Integer.toString(ourTime % 60) + " секунд(ы)";
+        }
+        else{
+            time = Integer.toString(ourTime % 60) + " секунд(ы)";
+        }
+        return time;
+    }
+    //Если была введена неправильная буква метод возвратит 1, нужно в случае очистки Labels
+    public int getFlagValue(){
+        return (this.wrongLit) ? 1 : 0;}
 
-    public int getFlagValue(){ return (this.flag) ? 1 : 0;}
+    public void clearWrongLit(){
+        this.wrongLit = false;
+    }
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -49,57 +61,27 @@ public class KeyCatcher extends JFrame implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-
-        //System.out.println(txtf.getText());
-        //System.out.println(labels[catcher].getText());
-//        if (Objects.equals(catcher, labels.length)){
-//            System.out.println("Done");
-//            //JOptionPane.showMessageDialog(BoxLayoutPanel.this,"Ля, КРАСАВА!");
-//        }
-//        else
+        if (!flag){
+            this.startTime = System.currentTimeMillis();
+            flag = !flag;
+        }
         if (Objects.equals(Character.toString(txtf.getText().toCharArray()[catcher]), labels[catcher].getText())){
             labels[catcher].setForeground(Color.GREEN);
             this.catcher +=1;
-            if (this.catcher == labelsSize){JOptionPane.showMessageDialog(txtf,
-                    "YRA!");}
+            if (this.catcher == labelsSize){
+                //this.catcher --;
+                String finishTime = "Поздравляю у вас ушло на набор каких то "+Integer.toString(this.labelsSize) +" букв всего то "+ timeMillstoSecAndMin((int)(System.currentTimeMillis() - startTime));
+                JOptionPane.showMessageDialog(txtf,
+                    finishTime);}
         }
         else{
-
             labels[catcher].setForeground(Color.RED);
-            flag = true;
+            wrongLit = true;
             JOptionPane.showMessageDialog(txtf,
                     "Введена лишняя буква",
                     "Inane error",
                     JOptionPane.ERROR_MESSAGE);
         }
 
-//        if( Objects.equals(Character.toString(txtf.getText().toCharArray()[catcher]), labels[catcher].getText())){
-//                labels[catcher].setForeground(Color.GREEN);
-//                this.catcher +=1;
-//            }
-//        if (this.catcher > txtf.getText().length()){ //Флаг который проверяет, исправляем ли мы строку, если начали исправлять, то false
-//            System.out.println("dfas");
-//            flag = !flag;
-//            labels[catcher].setForeground(Color.BLACK);
-//            //this.catcher -= 1;
-//        }
-//        else{
-//            flag = true;
-//        }
-//        if (flag){
-//            if( Objects.equals(Character.toString(txtf.getText().toCharArray()[catcher]), labels[catcher].getText())){
-//                labels[catcher].setForeground(Color.GREEN);
-//                this.catcher +=1;
-//            }
-//            else{
-//                labels[catcher].setForeground(Color.RED);
-//            }
-//        }
-
-
-//        else if(!Objects.equals(Character.toString(txtf.getText().toCharArray()[catcher]), labels[catcher].getText()) || !flag){
-//
-//            labels[catcher].setForeground(Color.RED);
-//        }
     }
 }
